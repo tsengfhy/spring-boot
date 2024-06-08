06f8c1b624a1d3715f6aa0789b93b5b63dfd36ba
Tsengfhy <tsengfhy@gmail.com>
2024-06-08 11:28:27 +0800

    Sample

---
+++ spring-boot-project/spring-boot-starters/spring-boot-starter-sample/src/main/java/org/springframework/boot/starter/sample/Application.java
@@ -0,0 +1,12 @@
+package org.springframework.boot.starter.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
